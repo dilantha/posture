1a6f472e07be87036aa719fcb95419d1c1c9ea59
Dilantha Nanayakkara <450922+dilantha@users.noreply.github.com>
2022-09-08 08:29:59 +1000

    Add post route and test

--- posture.go
+++ posture.go
@@ -14,9 +14,15 @@ func main() {
 func setupRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/", show)
+	router.POST("/", create)
 	return router
 }
 
 func show(c *gin.Context) {
 	c.String(http.StatusOK, "Posture Data")
+}
+
+func create(c *gin.Context) {
+	var posture string = c.PostForm("posture")
+	c.String(http.StatusCreated, "Created posture=%s", posture)
 }
--- posture_test.go
+++ posture_test.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetRoute(t *testing.T) {
+func TestShow(t *testing.T) {
 	router := setupRouter()
 
 	w := httptest.NewRecorder()
@@ -17,4 +19,18 @@ func TestGetRoute(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "Posture Data", w.Body.String())
+}
+
+func TestCreate(t *testing.T) {
+	router := setupRouter()
+
+	rec := httptest.NewRecorder()
+	data := url.Values{"posture": {"sit"}}
+	buffer := bytes.NewBufferString(data.Encode())
+	req, _ := http.NewRequest(http.MethodPost, "/", buffer)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(rec, req)
+
+	assert.Equal(t, http.StatusCreated, rec.Code)
+	assert.Equal(t, "Created posture=sit", rec.Body.String())
 }
